f91224a123293665cfad9286053614af9efb1333
Msingh0001 <c1s9major92@gmail.com>
2026-04-06 22:53:52 -0500

    Implemented the tip calculator

--- app/src/main/java/com/manvinder/myapplication/MainActivity.kt
+++ app/src/main/java/com/manvinder/myapplication/MainActivity.kt
@@ -1,20 +1,97 @@
 package com.manvinder.myapplication
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import android.text.TextWatcher
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+
+                Log.i(TAG, "onProgressChanged $progress")
+                tvTipPercentLabel.text = "$progress%"
+                computeTipAndTotal()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+        etBaseAmount.addTextChangedListener(object: TextWatcher {
+
+
+            // 1. Added the missing comma after CharSequence?
+            // 2. Fixed the parameter names (Android expects 'count, after' here)
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanged $s")
+                computeTipAndTotal()
+            }
+
+
+        })
+
+
+    }
+    private fun computeTipAndTotal() {
+        if (etBaseAmount.text.isEmpty())
+        { tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return }
+        val baseAmount =  etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = baseAmount + tipAmount
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
     }
 }
